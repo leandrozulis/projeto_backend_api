fe68a8b1627404374c789b5eabfe545322da9127
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2022-12-26 17:57:15 -0300

    moongose

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
 
     constructor() {
         this.server = express();
+
+        mongoose.set('strictQuery', false)
+
+        mongoose
+        .connect('mongodb+srv://leandrozulis:Le@1leandrinho@devhouse.emgyr0t.mongodb.net/?retryWrites=true&w=majority');
         
         this.middlewares();
         this.routes();

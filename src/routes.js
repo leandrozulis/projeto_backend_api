90c6efbddf7af1fdc60c704c241f0cb3e5cafb30
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-01-03 17:53:55 -0300

    dashboard e reserve

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
+import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 import UploadConfig from '../config/upload';
 import multer from 'multer';
 
@@ -16,4 +18,9 @@ routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
 
+//Dashboard
+routes.get('/dashboard', DashboardController.show);
+
+//Reserve
+routes.post('/houses/:house_id/reserve', ReserveController.store);
 export default routes;

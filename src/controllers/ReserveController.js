0e6af9fea409fa66b83e41ff9eedaa0981f6cd1e
leandrozulis <leandro_zulli@hotmail.com>
2023-01-04 21:44:24 -0300

    Reservas realizadas

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -1,9 +1,39 @@
 import Reserve from '../models/Reserve';
+import User from '../models/User';
+import House from '../models/House';
 
 class ReserveController {
 
     async store(req, res) {
-        return res.json({ok: true})
+
+        const { user_id } = req.headers;
+        const { house_id } = req.params;
+        const { date } = req.body;
+
+        const house = await House.findById(house_id);
+        if (!house) {
+            return res.status(400).json({ Error: "Essa casa não existe!" });
+        }
+
+        if (house.status !== true) {
+            return res.status(400).json({ Error: "Solicitação indisponível!" });
+        }
+
+        const user = await User.findById(user_id);
+        if (String(user._id) === String(house.user)) {
+            return res.status(401).json({ Error: "Não autorizado!" });
+        }
+
+        const reserve = await Reserve.create({
+            user: user_id,
+            house: house_id,
+            date
+        });
+
+        await reserve.populate('user');
+        await reserve.populate('house');
+
+        return res.json(reserve);
     }
 
 

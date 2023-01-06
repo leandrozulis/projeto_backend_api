48acc2eff023708549f0db22ba157bedcb7e2c06
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-01-06 08:58:11 -0300

    Instalação do Yup e realizado as validações de schema passados

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -8,6 +8,7 @@
  */
 
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class SessionController {
 
@@ -18,7 +19,15 @@ class SessionController {
     }
 
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        });
+
         const { email } = req.body;
+
+        if(!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: 'Falha na validação.' })
+        }
 
         let user = await User.findOne({ email });
 

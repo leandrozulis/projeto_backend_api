74ac4920ab979473956cc88c51d7546e033a1d56
Leandro Zulli Silva <leandro.silva@tecnospeed.com.br>
2023-01-06 08:33:30 -0300

    Subindo o diretório novamente

--- config/upload.js
+++ config/upload.js
@@ -3,7 +3,7 @@ import path from "path";
 
 export default {
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'upload'),
+    destination: path.resolve(__dirname, '..', 'uploads'),
     filename: (req, file, cb) => {
       const ext = path.extname(file.originalname);
       const name = path.basename(file.originalname, ext);
